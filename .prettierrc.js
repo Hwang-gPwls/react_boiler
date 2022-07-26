ccba46eef5dded41a2235ba78f2ead8e75f64bd4
HyeJin_Hwang <h960502hhj@gmail.com>
2022-07-26 11:09:37 +0900

    feat: create boilerplate

---
+++ .prettierrc.js
@@ -0,0 +1,18 @@
+/**
+ * @name: .pretteierrc.js
+ * @description: Prettier Option
+ * @version: 2.0
+ */
+
+module.exports = {
+	printWidth: 80,
+	tabWidth: 1,
+	useTabs: true,
+	semi: true,
+	singleQuote: true,
+	jsxSingleQuote: true,
+	trailingComma: 'all',
+	jsxBracketSameLine: true,
+	bracketSpacing: true,
+	arrowParens: 'always',
+};
